b510c9abf13b3cde1797c720838b8a6dfce358de
Huan Nguyen <huancal1984@gmail.com>
2019-11-08 00:52:53 -0800

    skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDqR4P-JwBJRcwL9aJEcPMmu5pdRicIYuk",
+        authDomain: "trainschedule-535dc.firebaseapp.com",
+        databaseURL: "https://trainschedule-535dc.firebaseio.com",
+        projectId: "trainschedule-535dc",
+        storageBucket: "trainschedule-535dc.appspot.com",
+        messagingSenderId: "1007375178585",
+        appId: "1:1007375178585:web:0b87f4441a39cdabaf0a48"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+    const db = firebase.database
+
+})
